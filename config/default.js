c05b1a4c80976d0397d6019fb172c77b99c80f23
Esteban <esteban93torres@gmail.com>
2018-11-09 01:49:19 -0500

    assets

---
+++ config/default.js
@@ -0,0 +1,3 @@
+export default {
+  mapKey: 'pk.eyJ1Ijoic3RibjkzIiwiYSI6ImNqbzZubmdpdjBuMmEzcW5sZm9xdm0yZTAifQ.lQ6AoCmGjvZuQr2CMnDcrw'
+}
---
+++ src/views/Recent.vue
@@ -0,0 +1,55 @@
+<template>
+	<div :class="{container: !checkMobile, 'container-pc': !checkMobile }">
+  <v-list two-line>
+      <v-list-tile
+      	v-for="(item, index) in incidents"
+        :key="item._id"
+        avatar
+        @click="handleIncident(item._id)"
+      >
+        <v-list-tile-avatar>
+          <img :src="incidentIcon(item.properties.type)">
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.properties.type"></v-list-tile-title>
+          <v-list-tile-sub-title v-text="item.properties.date"></v-list-tile-sub-title>
+          <v-list-tile-sub-title v-text="item.properties.description"></v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+  </v-list>
+	</div>
+</template>
+<script>
+import {mapGetters} from 'vuex'
+export default  {
+	name: 'Recent',
+	data () {
+		return {
+			incidents: null
+		}
+	},
+	methods: {
+		fetchIncidents(){
+			fetch('http://localhost:3000/api/events',{mode: 'cors'})
+		    .then(res => res.json())
+		    .then(res => {
+		      this.incidents = res
+      })
+		},
+		incidentIcon(incidentType) {
+			return 'img/report/' + incidentType.replace(/ /g,'').toLowerCase() + '.png'
+		},
+		handleIncident(incidentId) {
+			this.$router.push(`/?incident=${incidentId}`)
+		}
+	},
+	 computed: {
+  	...mapGetters({
+      checkMobile: 'checkMobile'
+  	})
+	},
+	mounted () {
+		this.fetchIncidents()
+	}
+}
+</script>
